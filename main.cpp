cd09486b0980f0ee27b140a0efba6844c66c6f14
RenenKantor <renenkantor@campus.technion.ac.il>
2020-11-25 17:46:06 +0200

    renen update 2

--- main.cpp
+++ main.cpp
@@ -2,6 +2,8 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::cout << "renen change in middle is the best partner in fdsnfthe wowwrld." << std::endl;
+    std::cout << "wow";
     std::cout << "renen  ron ron ron is the ron is king best partner in fdsnfthe wowwrld." << std::endl;
 
     return 0;
