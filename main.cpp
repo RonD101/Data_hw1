4f7b4518f4f70c6627f0bbef1987d68015a2d1a0
RonD101 <ron.dahan@campus.technion.ac.il>
2020-11-25 17:48:48 +0200

    hell yeah

--- main.cpp
+++ main.cpp
@@ -3,8 +3,6 @@
 int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "renen change in middle is the best partner in fdsnfthe wowwrld." << std::endl;
-    std::cout << "wow";
-    std::cout << "renen  ron ron ron is the ron is king best partner in fdsnfthe wowwrld." << std::endl;
 
     return 0;
 }
