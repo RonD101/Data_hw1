b5ae05b7d86b3e245fee1284e08abe9859e36333
RonD101 <ron.dahan@campus.technion.ac.il>
2020-11-25 17:50:30 +0200

    hell yeah

--- main.cpp
+++ main.cpp
@@ -4,5 +4,7 @@ int main() {
     std::cout << "Hello, World!" << std::endl;
     std::cout << "renen change in middle is the best partner in fdsnfthe wowwrld." << std::endl;
 
+    std::cout << "FUCKKKKK" << std::endl;
+
     return 0;
 }
