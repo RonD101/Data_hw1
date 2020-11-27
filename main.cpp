5497e584bc7ed0591e7c88609fdb518fb65daeaa
RonD101 <ron.dahan@campus.technion.ac.il>
2020-11-27 15:45:57 +0200

    hell yeah

--- main.cpp
+++ main.cpp
@@ -1,11 +1,18 @@
 #include <iostream>
 
 #include "AVL.h"
+
+using std::cout;
+using std::endl;
+
 int main() {
     AVLTree<int> new_tree;
     new_tree.insert_value(5);
     new_tree.insert_value(4);
-
-    new_tree.inorder(new_tree.get_root());
+    new_tree.insert_value(3);
+    new_tree.insert_value(6);
+    new_tree.insert_value(7);
+    cout << new_tree.get_tree_height(new_tree.get_root()) << endl;
+//    new_tree.inorder(new_tree.get_root());
     return 0;
 }
