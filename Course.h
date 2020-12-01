d3400e60a8448d9e3ac1935c76a1391d78cde92b
RonD101 <ron.dahan@campus.technion.ac.il>
2020-12-01 15:38:18 +0200

    hell yeah

--- Course.h
+++ Course.h
@@ -2,19 +2,29 @@
 #define HW1_COURSE_H
 #include "AVL.h"
 #include "Lecture.h"
+#include "TemArray.h"
+
 class Course {
     public:
         explicit Course(int id) : id(id) {};
         int id;
-        AVLTree<Lecture>* lectures_tree;
-        bool operator==(const Course other) const {
+        TemArray<Lecture> lectures;
+
+        // Courses are ordered by id only.
+        bool operator==(const Course &other) const {
             return (id == other.id);
         }
-        bool operator<(const Course other) const {
+        bool operator<(const Course &other) const {
             return (id < other.id);
         }
-        bool operator>(const Course other) const {
+        bool operator>(const Course &other) const {
             return (id > other.id);
+        }
+        friend std::ostream& operator<<(std::ostream& os, const Course& c){
+            os << "Course ID : " << c.id << std::endl;
+            for(int i = 0; i < c.lectures.size(); i++)
+                os << c.lectures[i] << std::endl;
+            return os;
         }
 };
 #endif //HW1_COURSE_H
